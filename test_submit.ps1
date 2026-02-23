2df703c47529fff5fbeb682459085910673251ed
taxbridg-ie <irelandtaxbridge@gmail.com>
2026-02-23 22:55:30 +0000

    Add files via upload

---
+++ test_submit.ps1
@@ -0,0 +1,14 @@
+$url = 'https://forms.zohopublic.eu/infotaxa1/form/leadformwebsite/formperma/zXMKA2XCuiSvQN3yHRuMRdCFarznyuzBf95baI1E3-c/htmlRecords/submit'
+$data = @{
+    'Name_First'='Test';
+    'Name_Last'='User';
+    'Email'='test@example.com';
+    'PhoneNumber_countrycode'='1234567890';
+    'Dropdown'='FORM 12';
+}
+try {
+    $response = Invoke-RestMethod -Uri $url -Method Post -Body $data -ErrorAction Stop
+    Write-Output "Success"
+} catch {
+    Write-Output "Error: $_"
+}
